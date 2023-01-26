95b08f1fccd215c8555940879c2dec1b728b4d3c
kernitus <2789734+kernitus@users.noreply.github.com>
2023-01-26 22:19:34 +0000

    Rudimentary rate limit

--- player/src/main/java/xyz/gianlu/librespot/player/mixing/output/HttpOutput.java
+++ player/src/main/java/xyz/gianlu/librespot/player/mixing/output/HttpOutput.java
@@ -4,6 +4,7 @@ import com.sun.net.httpserver.HttpServer;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import xyz.gianlu.librespot.player.mixing.output.wave.RateLimitedOutputStream;
 import xyz.gianlu.librespot.player.mixing.output.wave.WavFile;
 
 import java.io.IOException;
@@ -62,13 +63,15 @@ public final class HttpOutput implements SinkOutput {
                 httpExchange.sendResponseHeaders(response, 0);
                 LOGGER.info("Sent response headers");
 
-                stream = httpExchange.getResponseBody();
+                final int byteRate = (int) (format.getChannels() * format.getSampleSizeInBits() * format.getSampleRate() / 8);
+                LOGGER.info("Byte rate: " + byteRate);
+                stream = new RateLimitedOutputStream(httpExchange.getResponseBody(), byteRate);
+                //stream = httpExchange.getResponseBody();
                 LOGGER.info("Opened response body");
                 WavFile.writeHeader(stream, format.getChannels(), format.getSampleSizeInBits(), (long) format.getSampleRate());
                 LOGGER.info("Wrote WAV header");
                 // Let the write method proceed
                 headerWritten.complete(true);
-                LOGGER.info("Released lock");
             });
 
             server.start();
@@ -94,9 +97,12 @@ public final class HttpOutput implements SinkOutput {
             throw new RuntimeException(e);
         }
 
-        // when on inactive session:
-        // ERROR <general>: CCurlFile::FillBuffer - Failed: Timeout was reached(28)
+        // Write to body stream that was opened in start()
+        stream.write(buffer);
 
+        // TODO find better way to sync playback
+        // as the playing keeps going, kodi falls behind what spotify reports as the playback
+        // is librespot's calculations for playback tied to how quickly we write out of the buffer?
         try {
             // This is around ~23 that with 4096 byte buffer is slightly faster than s16le 44100 2 channel playback data rate
             // Necessary otherwise we buffer too much into HTTP stream and pausing / skipping no longer match up
@@ -104,17 +110,14 @@ public final class HttpOutput implements SinkOutput {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
-        // Write to body stream that was opened in start()
-        stream.write(buffer, offset, len);
     }
 
     @Override
     public void close() throws IOException {
-        // TODO might have to close() httpexchange or consume requestbody
-        stream.close();
-        server.stop(0);
+        if(stream != null) stream.close();
+        if(server != null) server.stop(0);
         server = null;
         headerWritten = null;
+        LOGGER.info("HTTP stream has been closed");
     }
 }
---
+++ player/src/main/java/xyz/gianlu/librespot/player/mixing/output/wave/RateLimitedOutputStream.java
@@ -0,0 +1,66 @@
+package xyz.gianlu.librespot.player.mixing.output.wave;
+
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class RateLimitedOutputStream extends BufferedOutputStream {
+    // e.g. 44100 sample rate, 16 bits per sample, 2 channels is 44100 * 16 * 2 = 1411200 b/s = 176400 B/s = 176.4 B/ms
+    private static final Logger LOGGER = LoggerFactory.getLogger(RateLimitedOutputStream.class);
+    private final long bytesPerMillisecond;
+    private long startTime;
+    private long totalBytesWritten;
+    private final OutputStream stream;
+
+    public RateLimitedOutputStream(OutputStream stream, long bytesPerSecond) {
+        super(stream);
+        this.stream = stream;
+        this.bytesPerMillisecond = bytesPerSecond * 1000;
+        startTime = System.currentTimeMillis();
+        totalBytesWritten = 0;
+    }
+
+    @Override
+    public void write(byte @NotNull [] bytes) throws IOException {
+        write(bytes, 0, bytes.length);
+    }
+
+    @Override
+    public void write(byte @NotNull [] bytes, int offset, int length) throws IOException {
+        // Calculate the max amount of bytes we could have written
+        final long currentTime = System.currentTimeMillis();
+        final long elapsedTime = currentTime - startTime;
+        final long maxWrittenBytes = elapsedTime * bytesPerMillisecond;
+
+        // TODO deal with the totalBytesWritten overflowing
+        // TODO deal with underfilling of the buffer - if we write too slowly playback will skip
+
+        // Write first few bytes
+        final long byteBudget = Math.max(0, (maxWrittenBytes - totalBytesWritten));
+        final int toWrite = (int) Math.min(length, byteBudget);
+        stream.write(bytes, offset, toWrite);
+        totalBytesWritten += toWrite;
+        startTime = currentTime;
+
+        if (length > byteBudget) {
+            // Wait to write the rest
+            final int remainingBytes = length - (int) byteBudget;
+            LOGGER.debug("Remaining bytes: " + remainingBytes + " length: " + length + " budget:" + byteBudget);
+            final long msToWait = remainingBytes / bytesPerMillisecond;
+            if(msToWait > 0) {
+                try {
+                    LOGGER.debug("Waiting " + msToWait + " to write remaining " + remainingBytes);
+                    Thread.sleep(msToWait);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            // Call function again with remaining of array
+            write(bytes, toWrite, remainingBytes);
+        }
+    }
+}
